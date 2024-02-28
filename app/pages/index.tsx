f549b7210f281d116f1b3556b2b339f031e4a26a
SolomonFoskaay <oskfak@gmail.com>
2024-02-28 10:09:59 +0000

    Updated _app.tsx and index.tsx files

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,11 @@
 import { Box, Flex, Spacer, VStack } from "@chakra-ui/react";
-import WalletMultiButton from "@/components/WalletMultiButton";
+
 
 export default function Home() {
   return (
     <Box>
       <Flex px={4} py={4}>
         <Spacer />
-        <WalletMultiButton />
       </Flex>
       <VStack justifyContent="center">
       </VStack>
